84e8f82e92a8767b0244dc0a07dac26ed491aefa
fastsnowx <61731151+fastsnowx@users.noreply.github.com>
2022-12-16 07:48:29 +0000

    [WIP] 再度fetchしないようにキャッシュに保存したい

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,51 @@ import SiteTitle from './components/siteTitle';
 import Anime from './anime.json';
 import { MdDeleteForever } from 'react-icons/md';
 import _ from 'lodash';
-import {gql, useLazyQuery, DocumentNode} from '@apollo/client';
+import { gql, useLazyQuery, DocumentNode } from '@apollo/client';
 import { AppShell, Header, Navbar, Pagination } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 function App() {
 
   //annictAPIから受け取れるjsonファイルの中身の型定義
-  interface animeInterface{
+  interface animeInterface {
     __typename: string
-    annictId:number
-    malAnimeId:string
-    officialSiteUrl:string
+    annictId: number
+    malAnimeId: string
+    officialSiteUrl: string
     title: string
     twitterUsername: string
   }
 
   //検索前に表示するアニメのリストを作成
   const initial_anime = [];
-  for (let i = 0; i < Anime.searchWorks.nodes.length; i++){
+  for (let i = 0; i < Anime.searchWorks.nodes.length; i++) {
     initial_anime.push(Anime.searchWorks.nodes[i]);
   }
 
   //レコメンド結果表示に必要なフラグの定義
-  const [pushCount,setPushCount] = useState<number>(0);
+  const [pushCount, setPushCount] = useState<number>(0);
   //表示するアニメカードのページ数を管理している
-  const [numPage,setNumPage] = useState<number>(0);
+  const [numPage, setNumPage] = useState<number>(0);
   //現在表示しているページ番号を管理している
-  const [nowPage,setNowPage] = useState<number>(1);
+  const [nowPage, setNowPage] = useState<number>(1);
   //表示させるアニメを管理している
-  const [displayAnimeList,setDisplayAnimeList] = useState<Array<animeInterface>>(initial_anime);
+  const [displayAnimeList, setDisplayAnimeList] = useState<Array<animeInterface>>(initial_anime);
   //アニメカードの表示に必要な変数の定義
   //とりあえず初期値はテキトーなので直す余地あり
-  const [animeList,setAnimeList] = useState<Array<animeInterface>>(initial_anime);
-  const [val,setVal] = useState<Array<string>>([]);
-  const [likeId,setLikeId] = useState<Array<string>>([]);
-  const [SEARCH_ANIME,setSEARCH_ANIME] = useState<DocumentNode>(gql`
+  const [animeList, setAnimeList] = useState<Array<animeInterface>>(initial_anime);
+  const [val, setVal] = useState<Array<string>>([]);
+  const [likeId, setLikeId] = useState<Array<string>>([]);
+  const [SEARCH_ANIME, setSEARCH_ANIME] = useState<DocumentNode>(gql`
   query {
     searchWorks(
       orderBy: { field: WATCHERS_COUNT, direction: DESC },
@@ -65,7 +75,7 @@ function App() {
   const [inputAnime, { data }] = useLazyQuery(SEARCH_ANIME);
 
   //テキストボックスに入力された文字列を元にqueryを作成
-  const search = (value:string) => {
+  const search = (value: string) => {
     setSEARCH_ANIME(gql`
     query {
       searchWorks(
@@ -96,34 +106,34 @@ function App() {
   const valChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (val.includes(e.target.value)) {
       setVal(val.filter(item => item !== e.target.value));
-    }else {
-      setVal([...val,e.target.value]);
+    } else {
+      setVal([...val, e.target.value]);
     }
     if (likeId.includes(e.target.id)) {
       setLikeId(likeId.filter(item => item !== e.target.id));
-    }else {
-      setLikeId([...likeId,e.target.id]);
+    } else {
+      setLikeId([...likeId, e.target.id]);
     }
   }
 
   //選択しているアニメカードを管理している(選択されたアニメ一覧にあるボタンを押した際に実行)
-  const valChangeBtn = (e:React.MouseEvent<HTMLButtonElement>) => {
+  const valChangeBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (val.includes(e.currentTarget.value)) {
       setVal(val.filter(item => item !== e.currentTarget.value));
-    }else {
-      setVal([...val,e.currentTarget.value]);
+    } else {
+      setVal([...val, e.currentTarget.value]);
     }
     if (likeId.includes(e.currentTarget.id)) {
       setLikeId(likeId.filter(item => item !== e.currentTarget.id));
-    }else {
-      setLikeId([...likeId,e.currentTarget.id]);
+    } else {
+      setLikeId([...likeId, e.currentTarget.id]);
     }
   }
 
   //選択されたアニメ一覧を表示する
   //確認用なので後で消す
   const valDisplay = () => [
-    setPushCount(pushCount+1)
+    setPushCount(pushCount + 1)
   ]
 
   //検索した結果出てきたアニメの情報をリストに格納してる
@@ -131,63 +141,65 @@ function App() {
     if (data) {
       console.log(data)
       const li = [];
-      for (let i = 0; i < data.searchWorks.nodes.length; i++){
+      for (let i = 0; i < data.searchWorks.nodes.length; i++) {
         li.push(data.searchWorks.nodes[i]);
       }
       setAnimeList(li);
       const lenAnimeList = li.length;
-      setNumPage((lenAnimeList > 10)?Math.ceil(lenAnimeList/10):0)
-      setDisplayAnimeList(li.slice(0,10))
+      setNumPage((lenAnimeList > 10) ? Math.ceil(lenAnimeList / 10) : 0)
+      setDisplayAnimeList(li.slice(0, 10))
       console.log(animeList)
     }
   }
 
   //ページ遷移をした際の処理
-  const changePage = (page:number) =>{
+  const changePage = (page: number) => {
     const end = (page) * 10;
     const start = end - 10;
     setNowPage(page);
-    setDisplayAnimeList(animeList.slice(start,end));
+    setDisplayAnimeList(animeList.slice(start, end));
   }
 
   //data(graphQLの実行結果)の値が変わる度にmakeAnimeList関数が実行される
-  useEffect(makeAnimeList,[data])
+  useEffect(makeAnimeList, [data])
 
   return (
     <div className='App'>
-      <AppShell 
-        navbar={<Navbar width={{base:200}}>
-                  <SearchBox onChange={_.debounce((e) => handleChange(e),500)}/>
-                  <Sidebar setSearchAnime={setSEARCH_ANIME} setNowPage={setNowPage} inputAnime={inputAnime}/>
-                </Navbar>}
-        header={<Header height={60}><SiteTitle/></Header>}>
+      <AppShell
+        navbar={<Navbar width={{ base: 200 }}>
+          <SearchBox onChange={_.debounce((e) => handleChange(e), 500)} />
+          <Sidebar setSearchAnime={setSEARCH_ANIME} setNowPage={setNowPage} inputAnime={inputAnime} />
+        </Navbar>}
+        header={<Header height={60}><SiteTitle /></Header>}>
         <div className="main">
           <div className='animebox'>
             <div className='animes'>
-              {displayAnimeList.map((info,index) => {
+              {displayAnimeList.map((info, index) => {
                 return (
-                <MalCard annictID={info.annictId} malAnimeId={info.malAnimeId}  officialSiteUrl={info.officialSiteUrl} animeTitle={info.title} twitterUsername={info.twitterUsername} value={info.title} onChange={valChange} checked={val.includes(info.title)} key={index}/>)
+                  <QueryClientProvider client={queryClient}>
+                    <MalCard annictID={info.annictId} malAnimeId={info.malAnimeId} officialSiteUrl={info.officialSiteUrl} animeTitle={info.title} twitterUsername={info.twitterUsername} value={info.title} onChange={valChange} checked={val.includes(info.title)} key={info.malAnimeId} />
+                  </QueryClientProvider>)
               })}
             </div>
-            <Pagination total={numPage} position="center" onChange={(page:number) => changePage(page)} page={nowPage}/>
+            <Pagination total={numPage} position="center" onChange={(page: number) => changePage(page)} page={nowPage} />
           </div>
           <div>
             現在選択中のアニメ
           </div>
           <div className='selectAnimeBox'>
             <ul>
-              {val.map((title,index) =>
-              <li value={title} id={likeId[index]}>
-                {title}
-                <button className="deleteBtn" onClick={valChangeBtn} value={title} id={likeId[index]}>
-                  <MdDeleteForever className='deleteIcon'/>
-                </button>
-              </li>
+              {val.map((title, index) =>
+                <li value={title} id={likeId[index]}>
+                  {title}
+                  <button className="deleteBtn" onClick={valChangeBtn} value={title} id={likeId[index]}>
+                    <MdDeleteForever className='deleteIcon' />
+                  </button>
+                </li>
               )}
             </ul>
           </div>
-          <SearchButton onClick={valDisplay}/>
-          <ResultAnime pushCount={pushCount} likeList={likeId}/>
+          <SearchButton onClick={valDisplay} />
+          <ResultAnime pushCount={pushCount} likeList={likeId} />
         </div>
       </AppShell>
     </div>
--- src/components/malCard.tsx
+++ src/components/malCard.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import styles from './malCard.module.css';
 import { CgWebsite } from 'react-icons/cg';
 import { FaTwitter } from 'react-icons/fa';
-import { Card, Group, Image, NavLink, Text, Tooltip} from '@mantine/core'
+import { Card, Group, Image, NavLink, Text, Tooltip } from '@mantine/core'
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 
 //アニメカードのコンポーネント
 //画像リンクが存在しない場合・リンクが無効の場合の例外処理実装したい←実装済み
@@ -20,50 +21,66 @@ import { Card, Group, Image, NavLink, Text, Tooltip} from '@mantine/core'
 //@return 画像URLが存在しない場合 - NoImageと書かれた画像が表示される
 
 type Props = {
-  annictID:number;
-  malAnimeId:string;
-  officialSiteUrl:string;
-  animeTitle:string;
-  twitterUsername:string;
-  value:string;
-  onChange:(e: React.ChangeEvent<HTMLInputElement>) => void;
-  checked:boolean;
+  annictID: number;
+  malAnimeId: string;
+  officialSiteUrl: string;
+  animeTitle: string;
+  twitterUsername: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  checked: boolean;
 }
-
-const MalCard: React.FC<Props> = ({annictID,malAnimeId,officialSiteUrl,animeTitle,twitterUsername,value,onChange,checked}) => {
-  const [imgUrl,setImgUrl] = useState<string>(`${process.env.PUBLIC_URL}/noimage.png`)
+const MalCard: React.FC<Props> = ({ annictID, malAnimeId, officialSiteUrl, animeTitle, twitterUsername, value, onChange, checked }) => {
+  const [imgUrl, setImgUrl] = useState<string>(`${process.env.PUBLIC_URL}/noimage.png`)
   const ID = String(annictID);
   const twitterLink = `https://twitter.com/${twitterUsername}`;
 
   //画像取得を行うAPIの実行
-  useEffect(() => {
-    const access_api = async() => {
-      const response = await fetch(`https://dev-recoani-d6gutf2s.onrender.com/api/mal/image?malAnimeId=${malAnimeId}`,{
-        method:'GET',})
-        if (!response.ok){
-          const err = await response.json()
-          throw new Error(err)
-        }
+
+  const { data, refetch } = useQuery(
+    [`${malAnimeId}`],
+    async () => {
+      const response = await fetch(`https://dev-recoani-d6gutf2s.onrender.com/api/mal/image?malAnimeId=${malAnimeId}`, {
+        method: 'GET',
+      })
       const data = await response.json();
       const url = data.data[0]['url']
       setImgUrl(url)
-    }
-    access_api();
-    },[malAnimeId])
+    },
+    { staleTime: Infinity, cacheTime: Infinity },
+  );
+  // refetch();
+  useEffect(() => { refetch() }, [malAnimeId])
+  // useEffect(() => {
+  //   const access_api = async() => {
+  //     const response = await fetch(`https://dev-recoani-d6gutf2s.onrender.com/api/mal/image?malAnimeId=${malAnimeId}`,{
+  //       method:'GET',})
+  //       if (!response.ok){
+  //         const err = await response.json()
+  //         throw new Error(err)
+  //       }
+  //       const data = await response.json();
+  //       const url = data.data[0]['url']
+  //       setImgUrl(url)
+  //     }
+  //     access_api();
+  //   },[malAnimeId])
+
+
   return (
     <div className={styles.card}>
-      <input type="checkbox" id={ID} value={value} onChange={onChange} checked={checked}/>
+      <input type="checkbox" id={ID} value={value} onChange={onChange} checked={checked} />
       <Card withBorder radius="md" p={0} shadow="sm" component='label' htmlFor={ID}>
         <Group noWrap spacing={0}>
-          <Image src={imgUrl} height={140} width={100}/>
+          <Image src={imgUrl} height={140} width={100} />
           <div>
             <Tooltip label={animeTitle} multiline width={150}>
               <Text mt="xs" mb="md" className={styles.title}>
                 {animeTitle}
               </Text>
             </Tooltip>
-            <NavLink component="a" href={officialSiteUrl} rel="noreferrer" target="_blank" label="公式サイト" icon={<CgWebsite size={15}/>} />
-            <NavLink component="a" href={twitterLink} rel="noreferrer" target="_blank" label="Twitter" icon={<FaTwitter size={15}/>} />
+            <NavLink component="a" href={officialSiteUrl} rel="noreferrer" target="_blank" label="公式サイト" icon={<CgWebsite size={15} />} />
+            <NavLink component="a" href={twitterLink} rel="noreferrer" target="_blank" label="Twitter" icon={<FaTwitter size={15} />} />
             <div className={`${styles.LikesIcon} ${styles.HeartAnimation}`}></div>
           </div>
         </Group>
